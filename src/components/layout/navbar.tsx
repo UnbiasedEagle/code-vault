7fa95c619a91796c7b9c9ec0202b59f7377d1cdf
Saurabh Singh <imsaurabhsingh007@gmail.com>
2024-12-06 07:01:08 +0530

    make navbar responsive

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -3,14 +3,16 @@ import { Button } from '../ui/button';
 
 const Navbar = () => {
   return (
-    <nav className='flex h-20 items-center justify-between'>
+    <nav className='flex flex-col sm:flex-row mt-10 sm:mt-0 h-20 items-center justify-between'>
       <div className='flex items-center gap-2'>
         <FaCode className='text-primary' size={32} />
         <span className='font-bold text-2xl text-primary'>CodeVault</span>
       </div>
-      <div className='flex gap-2'>
-        <Button>Sign In</Button>
-        <Button variant='secondary'>Sign Up</Button>
+      <div className='flex flex-col w-full sm:w-auto mt-8 sm:mt-0 sm:flex-row sm:items-center gap-4 sm:gap-2'>
+        <Button className='w-full sm:w-auto'>Sign In</Button>
+        <Button className='w-full sm:w-auto' variant='secondary'>
+          Sign Up
+        </Button>
       </div>
     </nav>
   );
