4f05b5939bcbdc75d3b03428cbd32ebd38817d23
Saurabh Singh <imsaurabhsingh007@gmail.com>
2024-12-08 17:22:33 +0530

    add search bar in topbar

--- src/app/codes/page.tsx
+++ src/app/codes/page.tsx
@@ -1,9 +1,21 @@
-import { UserButton } from '@clerk/nextjs';
+import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
+import { TopBar } from './components/topbar';
 
-const CodesPage = () => {
+const CodesPage = async () => {
+  const user = await currentUser();
+
+  if (!user) {
+    return redirect('/sign-in');
+  }
+
   return (
     <div>
-      <UserButton />
+      <TopBar
+        imageUrl={user.imageUrl}
+        fullName={user.firstName + ' ' + user.lastName}
+        email={user.emailAddresses[0].emailAddress}
+      />
     </div>
   );
 };
