18e2fe74aa85ab4d2eafcd1592c0fa69577e717d
Saurabh Singh <imsaurabhsingh007@gmail.com>
2024-12-09 09:31:36 +0530

    implement tags swiper component

---
+++ src/app/codes/components/tags-swiper.tsx
@@ -0,0 +1,87 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { Card, CardContent } from '@/components/ui/card';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from '@/components/ui/carousel';
+import { Plus } from 'lucide-react';
+
+export const TagsSwiper = () => {
+  return (
+    <Card>
+      <CardContent className='p-4'>
+        <div className='flex relative items-center gap-2 overflow-hidden'>
+          <div
+            className='absolute overflow-hidden'
+            style={{
+              width: 'calc(100% - 80px)',
+            }}
+          >
+            <Carousel
+              opts={{
+                dragFree: true,
+              }}
+              style={{
+                width: 'calc(100% - 80px)',
+              }}
+            >
+              <CarouselContent className='flex space-x-4 text-neutral-400 font-medium'>
+                <CarouselItem>
+                  <Button>All</Button>
+                </CarouselItem>
+                <CarouselItem>
+                  <Button variant='ghost'>JavaScript</Button>
+                </CarouselItem>
+                <CarouselItem>
+                  <Button variant='ghost'>React</Button>
+                </CarouselItem>
+                <CarouselItem>
+                  <Button variant='ghost'>Node.js</Button>
+                </CarouselItem>
+                <CarouselItem>
+                  <Button variant='ghost'>Python</Button>
+                </CarouselItem>
+                <CarouselItem>
+                  <Button variant='ghost'>TypeScript</Button>
+                </CarouselItem>
+                <CarouselItem>
+                  <Button variant='ghost'>Vue.js</Button>
+                </CarouselItem>
+                <CarouselItem>
+                  <Button variant='ghost'>Angular</Button>
+                </CarouselItem>
+                <CarouselItem>
+                  <Button variant='ghost'>Django</Button>
+                </CarouselItem>
+                <CarouselItem>
+                  <Button variant='ghost'>Flask</Button>
+                </CarouselItem>
+                <CarouselItem>
+                  <Button variant='ghost'>Ruby on Rails</Button>
+                </CarouselItem>
+                <CarouselItem>
+                  <Button variant='ghost'>Spring</Button>
+                </CarouselItem>
+                <CarouselItem>
+                  <Button variant='ghost'>Laravel</Button>
+                </CarouselItem>
+                <CarouselItem>
+                  <Button variant='ghost'>Express</Button>
+                </CarouselItem>
+              </CarouselContent>
+            </Carousel>
+          </div>
+          <div className='ml-auto'>
+            <Button className='flex items-center gap-1'>
+              <Plus className='h-5 w-5' />
+              Tag
+            </Button>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
--- src/app/codes/page.tsx
+++ src/app/codes/page.tsx
@@ -1,6 +1,7 @@
 import { currentUser } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
 import { TopBar } from './components/topbar';
+import { TagsSwiper } from './components/tags-swiper';
 
 const CodesPage = async () => {
   const user = await currentUser();
@@ -10,12 +11,13 @@ const CodesPage = async () => {
   }
 
   return (
-    <div>
+    <div className='flex flex-col gap-5'>
       <TopBar
         imageUrl={user.imageUrl}
         fullName={user.firstName + ' ' + user.lastName}
         email={user.emailAddresses[0].emailAddress}
       />
+      <TagsSwiper />
     </div>
   );
 };
