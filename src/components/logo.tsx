56c08bddce0f0afebe9a172cd24f7026574263f2
Saurabh Singh <imsaurabhsingh007@gmail.com>
2024-12-22 20:42:48 +0530

    add link around logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,14 +1,16 @@
 'use client';
 
+import Link from 'next/link';
 import { FaCode } from 'react-icons/fa';
 
 export const Logo = () => {
   return (
-    <div className='flex items-center gap-2 px-6 h-16 border-b border-border'>
+    <Link
+      href='/codes'
+      className='flex items-center gap-2 px-6 h-16 border-b border-border'
+    >
       <FaCode className='text-primary' size={24} />
-      <span className='font-bold text-xl text-primary'>
-        CodeVault
-      </span>
-    </div>
+      <span className='font-bold text-xl text-primary'>CodeVault</span>
+    </Link>
   );
 };
