f4855d4ebf331c218a7fde3e2f3790f299b56e20
Saurabh Singh <imsaurabhsingh007@gmail.com>
2025-01-05 13:08:29 +0530

    fix signout functionality

--- src/components/signout-btn.tsx
+++ src/components/signout-btn.tsx
@@ -3,13 +3,16 @@
 import { Button } from '@/components/ui/button';
 import { useClerk } from '@clerk/nextjs';
 import { LogOut } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 
 export const SignOutBtn = () => {
+  const router = useRouter();
   const { signOut } = useClerk();
   return (
     <Button
       onClick={() => {
         signOut({ redirectUrl: '/' });
+        router.push('/');
       }}
       asChild
       variant='ghost'
