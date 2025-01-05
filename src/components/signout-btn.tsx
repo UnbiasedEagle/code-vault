92f303357920727c42a905844ff2eba5943b0258
Saurabh Singh <imsaurabhsingh007@gmail.com>
2025-01-05 12:46:52 +0530

    fix signout position

--- src/components/signout-btn.tsx
+++ src/components/signout-btn.tsx
@@ -6,17 +6,17 @@ import { LogOut } from 'lucide-react';
 
 export const SignOutBtn = () => {
   return (
-    <Button
-      asChild
-      variant='ghost'
-      className='w-full cursor-pointer justify-start text-muted-foreground hover:text-foreground'
-    >
-      <SignOutButton redirectUrl='/'>
+    <SignOutButton redirectUrl='/'>
+      <Button
+        asChild
+        variant='ghost'
+        className='w-full cursor-pointer justify-start text-muted-foreground hover:text-foreground'
+      >
         <div className='flex items-center gap-3'>
           <LogOut size={16} />
           Sign out
         </div>
-      </SignOutButton>
-    </Button>
+      </Button>
+    </SignOutButton>
   );
 };
